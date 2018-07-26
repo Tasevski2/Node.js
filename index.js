c4a95f6fb1b27b68eb44b986cb9286a97f769f73
Tasevski2 <39170279+Tasevski2@users.noreply.github.com>
2018-07-26 21:34:56 +0200

    cas1

---
+++ index.js
@@ -0,0 +1,49 @@
+// var name = "ViktOR";
+// console.log(name.toLowerCase());
+// var nameUpdated = name.toLowerCase();
+
+// if(nameUpdated=="viktor")
+// {
+// 	console.log("Hello " + name);
+// }
+// else
+// 	console.log("You are not " + name);
+
+
+// switch(name)
+// {
+// 	case "Viktor":
+// 	console.log("Hello " + name);
+// 	break;
+// 	case "viktor":
+// 	console.log("Hello " + name);
+// 	break;
+// 	default:
+// 	console.log("You are not " + name);
+// }
+
+// var firstName = "Viktor ";
+// var lastName = "Tasevski";
+// var fullName = firstName.concat(lastName);
+// console.log(fullName);
+
+// var boolean = true;
+
+var integer = [3,4,5,6,7,8,9,10,12];
+
+integer[100] = 50;
+console.log(integer);
+
+// for(var i=0;i<integer.length;i++)
+// {	
+// 	if(integer[i]%3==0 && integer[i]%4==0)
+// 		console.log("buzz fiz");
+// 	else if(integer[i]%3==0)
+// 		console.log("buzz");
+// 	else if(integer[i]%4==0)
+// 		console.log("fiz");
+// 	else
+// 		console.log("");
+// }
+
+
